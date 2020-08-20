c88c4228e6bd18ee66c14853afb984fbcc36072f
Andrei Voicu <dreivoicu@gmail.com>
2020-08-20 16:48:08 +0300

    shuffle the array

---
+++ src/easy/shuffle-the-array.js
@@ -0,0 +1,14 @@
+function shuffle(nums, n) {
+  const secondHalf = nums.splice(n);
+
+  return nums.reduce((sum, curr, i) => {
+    return sum.concat(curr, secondHalf[i]);
+  }, []);
+}
+
+console.log(shuffle([2, 5, 1, 3, 4, 7], 3));
+// [2,3,5,4,1,7]
+console.log(shuffle([1, 2, 3, 4, 4, 3, 2, 1], 4));
+// [1,4,2,3,3,2,4,1]
+console.log(shuffle([1, 1, 2, 2], 2));
+// [1,2,1,2]
