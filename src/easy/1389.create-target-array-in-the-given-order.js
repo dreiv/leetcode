0642fa396030a7cdb3ccad40a6f418c18b43fb3b
Andrei Voicu <dreivoicu@gmail.com>
2020-08-22 16:22:49 +0300

    Create Target Array in the Given Order

---
+++ src/easy/1389.create-target-array-in-the-given-order.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/create-target-array-in-the-given-order/
+
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function (nums, index) {
+  const result = [];
+
+  for (const i in nums) {
+    result.splice(index[i], 0, nums[i]);
+  }
+
+  return result;
+};
+
+console.log(createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+// [0,4,1,3,2]
+console.log(createTargetArray([1, 2, 3, 4, 0], [0, 1, 2, 3, 0]));
+// [0,1,2,3,4]
+console.log(createTargetArray([1], [0]));
+// [1]
