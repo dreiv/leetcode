077c29c386f2d5bf9b32cd5be95b6d7acd3f4257
Andrei Voicu <dreivoicu@gmail.com>
2020-08-23 15:16:04 +0300

    Minimum Time Visiting All Points

---
+++ src/easy/1266.minimum-time-visiting-all-points.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/minimum-time-visiting-all-points/
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function (points) {
+  let visitTime = 0;
+
+  for (let i = 0; i < points.length - 1; i++) {
+    const [x1, y1] = points[i];
+    const [x2, y2] = points[i + 1];
+
+    visitTime += Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
+  }
+
+  return visitTime;
+};
+
+console.log(
+  minTimeToVisitAllPoints([
+    [1, 1],
+    [3, 4],
+    [-1, 0]
+  ])
+);
+// 7
+console.log(
+  minTimeToVisitAllPoints([
+    [3, 2],
+    [-2, 2]
+  ])
+);
+// 5
