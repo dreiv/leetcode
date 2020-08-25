23fc32c28003b6aa1053a57ff82f476222b8339c
Andrei Voicu <dreivoicu@gmail.com>
2020-08-26 00:09:23 +0300

    Destination City 2

--- src/easy/1436.destination-city.js
+++ src/easy/1436.destination-city.js
@@ -5,8 +5,8 @@
  * @return {string}
  */
 function destCity(paths) {
-  const set = new Set();
-  for (const path of paths) set.add(path[0]);
+  const set = new Set(paths.map((path) => path[0]));
+
   for (const path of paths) {
     if (!set.has(path[1])) return path[1];
   }
