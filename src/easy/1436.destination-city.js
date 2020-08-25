00613dbdf7a193a2398bd99e12dbc6e94e46397a
Andrei Voicu <dreivoicu@gmail.com>
2020-08-25 23:54:40 +0300

    Destination City

---
+++ src/easy/1436.destination-city.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/destination-city/
+
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+function destCity(paths) {
+  const set = new Set();
+  for (const path of paths) set.add(path[0]);
+  for (const path of paths) {
+    if (!set.has(path[1])) return path[1];
+  }
+}
+
+console.log(
+  destCity([
+    ['London', 'New York'],
+    ['New York', 'Lima'],
+    ['Lima', 'Sao Paulo']
+  ])
+);
+// "Sao Paulo"
+console.log(
+  destCity([
+    ['B', 'C'],
+    ['D', 'B'],
+    ['C', 'A']
+  ])
+);
+// "A"
+console.log(destCity([['A', 'Z']]));
+// "Z"
